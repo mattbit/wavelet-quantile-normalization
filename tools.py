9ccfe6d604819f0c988f7c73f9b085a5e8f2c1d2
Matteo <matteo.dora@ens.fr>
2022-02-12 20:10:20 +0100

    Update tools

--- tools.py
+++ tools.py
@@ -1,14 +1,10 @@
 import numpy as np
+import pandas as pd
 import scipy.signal as ss
 from scipy.stats import entropy
 
 
-def psd(signal):
-    f, P = ss.welch(signal, nperseg=256, noverlap=128, axis=1)
-    return f, P.mean(axis=0)
-
-
-def calculate_metrics(restored, signal, reference, artifact_mask):
+def calculate_metrics(restored, signal, reference, artifact_mask, freq=None):
     _res = restored[:, artifact_mask]
     _ref = reference[:, artifact_mask]
     _sig = signal[:, artifact_mask]
@@ -26,28 +22,29 @@ def calculate_metrics(restored, signal, reference, artifact_mask):
     r_after = calculate_correlation(restored, reference)
     delta_r = r_after - r_before
 
-    # Spectral features
-    _, P_sig = psd(signal)
-    _, P_ref = psd(reference)
-    _, P_res = psd(restored)
-
-    sp_snr_before = calculate_snr(P_sig, P_sig - P_ref)
-    sp_snr_after = calculate_snr(P_ref, P_res - P_ref)
-    sp_delta_snr = sp_snr_after - sp_snr_before
+    # Spectral coherence
+    c_max_freq = 40
+    ff, coh_before = ss.coherence(signal, reference, fs=freq, nperseg=128)
+    _, coh_after = ss.coherence(restored, reference, fs=freq, nperseg=128)
+    delta_coh = (coh_after - coh_before)[:, (ff <= c_max_freq)].mean()
 
     # Mutual information
     mi = calculate_mutual_information(_ref, _res)
 
     return {
-        'SNR_before': snr_before,
-        'SNR_after': snr_after,
-        'ΔSNR': delta_snr,
-        'NMSE': nmse,
-        'R_before': r_before,
-        'R_after': r_after,
-        'ΔR': delta_r,
-        'Spectral ΔSNR': sp_delta_snr,
-        'MI': mi,
+        "SNR_before": snr_before,
+        "SNR_after": snr_after,
+        "ΔSNR": delta_snr,
+        "NMSE": nmse,
+        "R_before": r_before,
+        "R_after": r_after,
+        "ΔR": delta_r,
+        "ΔR_normalized": delta_r / (1 - r_before),
+        "Coh_before": coh_before[:, ff <= c_max_freq].mean(),
+        "Coh_after": coh_after[:, ff <= c_max_freq].mean(),
+        "ΔCoh": delta_coh,
+        "ΔCoh_normalized": delta_coh / (1 - coh_before[:, ff <= c_max_freq].mean()),
+        "MI": mi,
     }
 
 
@@ -61,7 +58,7 @@ def signal_to_artifact_ratio(signal, artifact):
 
 
 def calculate_nmse(reference, restored):
-    nmse = ((reference - restored)**2).sum() / (reference**2).sum()
+    nmse = ((reference - restored) ** 2).sum() / (reference**2).sum()
     return 10 * np.log10(nmse)
 
 
@@ -90,20 +87,19 @@ def calculate_correlation(signal, other):
 
 def filter_bandpass(signal, low, high, fs, order=2):
     Wn = 2 * np.array([low, high]) / fs
-    sos = ss.butter(order, Wn, btype='bandpass', output='sos')
+    sos = ss.butter(order, Wn, btype="bandpass", output="sos")
 
     return ss.sosfiltfilt(sos, signal, axis=0)
 
 
 def filter_highpass(signal, freq, fs, order=2):
     Wn = 2 * freq / fs
-    sos = ss.butter(order, Wn, btype='highpass', output='sos')
+    sos = ss.butter(order, Wn, btype="highpass", output="sos")
 
     return ss.sosfiltfilt(sos, signal, axis=0)
 
 
-def apply_artifact_removal(original_signals, corrupted_signals, func,
-                           **kwargs):
+def apply_artifact_removal(original_signals, corrupted_signals, func, **kwargs):
     restored_signals = []
     for original, corrupted in zip(original_signals, corrupted_signals):
         restored = corrupted.copy()
@@ -113,7 +109,7 @@ def apply_artifact_removal(original_signals, corrupted_signals, func,
 
         for n in range(original.shape[0]):
             s = corrupted[n][start:end]
-            refs = [corrupted[n][:start], corrupted[n][end:end + keep_len]]
+            refs = [corrupted[n][:start], corrupted[n][end : end + keep_len]]
             restored[n][start:end] = func(s, refs, **kwargs)
 
         restored_signals.append(restored)
@@ -161,3 +157,96 @@ def intervals_to_mask(intervals, size=None):
         mask[i:j] = True
 
     return mask
+
+
+def signal_to_frames(signal, frame_size=256, step_size=None, window=None):
+    """Create frames from a signal.
+    Parameters
+    ----------
+    signal : numpy.ndarray
+        The signal used to create the frames.
+    frame_size : int
+        The size of the frames (number of samples, default is 256).
+    step_size : int
+        Defines the distance between successive frames. If not specified,
+        a value of 1/4 of the frame size is used.
+    window : str
+        The window function used. Accepted values are those defined in the
+        `scipy.signal.get_window` funtion. If not specified, no window function
+        is used (equivalent to a `boxcar` rectangular window).
+    Returns
+    -------
+    frames : numpy.ndarray (num frames, frame size)
+        The resulting frames.
+    """
+    if step_size is None:
+        step_size = frame_size // 4
+
+    offsets = np.arange(0, len(signal) - frame_size + 1, step_size)
+    frames = np.zeros((len(offsets), frame_size))
+    for n, offset in enumerate(offsets):
+        frames[n] = signal[offset : offset + frame_size]
+
+    if window is not None:
+        frames *= ss.get_window(window, frame_size)
+
+    return frames
+
+
+def frames_to_signal(frames, step_size, window=None):
+    """Reconstruct a signal given a sequence of frames.
+    Parameters
+    ----------
+    frames : numpy.ndarray (num frames, frame size)
+        The frames from which the signal should be constructed.
+    step_size : int
+        Defines the distance between successive frames.
+    window : str
+        The window function used. Accepted values are those defined in the
+        `scipy.signal.get_window` funtion. If not specified, no window function
+        is used (equivalent to a `boxcar` rectangular window).
+    Returns
+    -------
+    signal : numpy.ndarray
+        The reconstructed signal.
+    """
+    num_frames, frame_size = frames.shape
+    signal_size = step_size * (num_frames - 1) + frame_size
+
+    signal = np.zeros(signal_size)
+    overlap = np.zeros(signal_size)
+
+    window_values = ss.get_window(window, frame_size) if window else 1
+
+    offsets = step_size * np.arange(num_frames)
+    for n, offset in enumerate(offsets):
+        signal[offset : offset + frame_size] += frames[n]
+        overlap[offset : offset + frame_size] += window_values
+
+    return signal / overlap
+
+
+def cca(x, y):
+    """Canonical Correlation Analysis."""
+    z = np.concatenate([x, y]).T
+    C = np.cov(z.T)
+
+    nx = x.shape[0]
+    ny = y.shape[0]
+    Cxx = C[:nx, :nx] + 1e-10 * np.diag(np.ones(nx))
+    Cxy = C[:nx, nx : nx + ny]
+    Cyx = Cxy.T
+    Cyy = C[nx : nx + ny, nx : nx + ny] + 1e-10 * np.diag(np.ones(nx))
+
+    Cxx_inv = np.linalg.inv(Cxx)
+    Cyy_inv = np.linalg.inv(Cyy)
+
+    w, v = np.linalg.eig(Cxx_inv @ Cxy @ Cyy_inv @ Cyx)
+    order = w.argsort()[::-1]
+    r = np.sqrt(w[order])
+    Wx = v[:, order]
+
+    Wy = Cyy_inv @ Cyx @ Wx
+    Wy /= np.sqrt((np.abs(Wy) ** 2).sum(axis=0))
+
+    return Wx, Wy, r
